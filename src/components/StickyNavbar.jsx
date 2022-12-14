cc01721c9a83cf00ab90097f4785ae81c0b073b6
Eliott Morcillo <eliott.morcillo@gmail.com>
2022-12-14 14:22:37 +0100

    feat: link to home on mobile

--- src/components/StickyNavbar.jsx
+++ src/components/StickyNavbar.jsx
@@ -3,6 +3,7 @@ import useDarkMode from "../lib/useDarkMode";
 import { BsGithub, BsSun } from "react-icons/bs";
 import { FiMoon } from "react-icons/fi";
 import Button from "./Button";
+import { Link } from "react-router-dom";
 const StickyNavbar = ({ children }) => {
   const [colorTheme, setTheme] = useDarkMode();
 
@@ -10,7 +11,9 @@ const StickyNavbar = ({ children }) => {
     <div className="sticky pl-10 z-10 p-3 pb-0 pt-4 top-0 left-0 right-0 flex items-center dark:bg-slate-700 bg-gray-100 border-b dark:border-b-slate-600 justify-end md:justify-between">
       <div className="flex-grow hidden md:flex ">{children}</div>
       <div className="md:hidden flex flex-grow m-0 absolute left-0 ml-4 top-[50%] -translate-y-1/2">
-        <Button defaultbtn={true}>Home</Button>
+        <Link to="/">
+          <Button defaultbtn={true}>Home</Button>
+        </Link>
       </div>
       <div className="flex items-center px-5 pb-4">
         {colorTheme === "light" ? (
